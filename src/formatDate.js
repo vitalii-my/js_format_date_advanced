6d6977838e03129c1c971d1239db1460ed33b906
vitalii <vitalii.myhailyk.w@gmail.com>
2025-02-05 04:45:15 +0200

    solved task

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,43 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSeparator = fromFormat[3];
+  const toSeparator = toFormat[3];
+  const fromDateArr = date.split(fromSeparator);
+  const fromFormatObj = {};
+  const toFormatArr = [];
+
+  for (let i = 0; i < 3; i++) {
+    fromFormatObj[fromFormat[i]] = fromDateArr[i];
+  }
+
+  const yearKey = fromFormatObj['YYYY'] ? 'YYYY' : 'YY';
+
+  /* switch (yearKey) {
+    case 'YYYY':
+      fromFormatObj['YY'] = fromFormatObj[yearKey].slice(2);
+      break;
+
+    case 'YY':
+      fromFormatObj['YYYY'] = +fromFormatObj[yearKey] < 30
+        ? `20${fromFormatObj[yearKey]}`
+        : `19${fromFormatObj[yearKey]}`;
+      break;
+  } */
+
+  if (yearKey === 'YY') {
+    fromFormatObj['YYYY'] = +fromFormatObj[yearKey] < 30
+      ? `20${fromFormatObj[yearKey]}`
+      : `19${fromFormatObj[yearKey]}`;
+  } else {
+    fromFormatObj['YY'] = fromFormatObj[yearKey].slice(2);
+  }
+
+  for (const value of toFormat.slice(0, 3)) {
+    toFormatArr.push(fromFormatObj[value]);
+  }
+
+  return toFormatArr.join(toSeparator);
 }
 
 module.exports = formatDate;
